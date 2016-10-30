eff0e6ccddeb43ae614161fc93164c25b8696303
jhon.calderon <jhon.calderon@udea.edu.co>
2016-10-30 15:55:07 -0500

    Se modifica las clase respuesta y se pule la funcion comparar

--- SAapp/app/src/main/java/com/pmovil/ui/saapp/MainActivity.java
+++ SAapp/app/src/main/java/com/pmovil/ui/saapp/MainActivity.java
@@ -7,74 +7,55 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView salida;
     public String entrada;
     public Respuesta res1;
-    int ocu=2;
+    int i=0;
+    int ocu=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //res1= new Respuesta("tiene","disponibles","unidades", "Cordial saludo, tengo disponibles para entrega inmediata");
-        res1=new Respuesta();
+        res1= new Respuesta("ESTA ES TU RESPUESTA SEÑOR PERESOSO","tiene","disponibles","unidades");
         findViewById(R.id.imageButton).setOnClickListener(this);
         findViewById(R.id.imageButton2).setOnClickListener(this);
-        res1.respuesta = ((TextView) findViewById(R.id.textView));
+        res1.respuesta = (TextView) findViewById(R.id.textView);
         res1.compara1 = (TextView) findViewById(R.id.textView2);
         res1.compara2 = (TextView) findViewById(R.id.textView3);
         res1.compara3 = (TextView) findViewById(R.id.textView4);
         salida = (TextView) findViewById(R.id.muestra);
-        //entrada = (EditText) findViewById(R.id.editText);
+        res1.respuesta.setText(res1.claves[0]);
+        res1.compara1.setText(res1.claves[1]);
+        res1.compara2.setText(res1.claves[2]);
+        res1.compara3.setText(res1.claves[3]);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.imageButton2:
-                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                entrada= String.valueOf(clipboard.getText());
-                ocu=ocurrencia(res1.compara1.getText().toString());
-                if (ocu!=-1) {
-                    salida.setText(res1.compara1.getText().toString());
-                    //Copiamos al portapapeles
-                    ClipData clip = ClipData.newPlainText("text",res1.respuesta.getText());
-                    clipboard = (ClipboardManager)this.getSystemService(CLIPBOARD_SERVICE);
-                    clipboard.setPrimaryClip(clip);
-                }
-                else {
-                    ocu = ocurrencia(res1.compara2.getText().toString());
-                    if (ocu!=-1) {
-                        salida.setText(res1.compara2.getText().toString());
-                        //Copiamos al portapapeles
-                        ClipData clip = ClipData.newPlainText("text",res1.respuesta.getText());
-                        clipboard = (ClipboardManager)this.getSystemService(CLIPBOARD_SERVICE);
-                        clipboard.setPrimaryClip(clip);
-                    }
-                    else
-                        ocu = ocurrencia(res1.compara3.getText().toString());
-                    if (ocu!=-1) {
-                        salida.setText(res1.compara3.getText().toString());
-                        //Copiamos al portapapeles
-                        ClipData clip = ClipData.newPlainText("text",res1.respuesta.getText());
-                        clipboard = (ClipboardManager)this.getSystemService(CLIPBOARD_SERVICE);
-                        clipboard.setPrimaryClip(clip);
-                    }
-                    else if (ocu==-1)
-                        salida.setText("No Encontrado"+res1.compara1.getText().toString()+res1.compara2.getText().toString()+res1.compara3.getText().toString());
-                }
-                    //Salida.setText("NO ENCONTRADO"+sText);
-                break;
-            case R.id.imageButton:
-                salida.setText("No Encontrado"+res1.compara2.getText().toString());
-                break;
+                i=0;
+                compara();
 
         }
     }
-
-    private int ocurrencia(String respuesta) {
-        int i = entrada.indexOf(respuesta);
-        return i;
+    private void compara() {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        entrada = String.valueOf(clipboard.getText());
+        while (ocu==-1 && i<=2){
+            ocu = entrada.indexOf(res1.claves[i]);
+            i++;
+        }
+        if (ocu !=-1) {
+            salida.setText(res1.claves[i]);
+            ClipData clip = ClipData.newPlainText("text", res1.respuesta.getText().toString());
+            clipboard = (ClipboardManager) this.getSystemService(CLIPBOARD_SERVICE);
+            clipboard.setPrimaryClip(clip);
+            }
+        else
+            salida.setText("No Encontrado");
     }
 }
--- SAapp/app/src/main/java/com/pmovil/ui/saapp/Respuesta.java
+++ SAapp/app/src/main/java/com/pmovil/ui/saapp/Respuesta.java
@@ -11,12 +11,12 @@ public class Respuesta {
     public TextView compara2;
     public TextView compara3;
     public TextView respuesta;
-   /* public Respuesta(String com1,String com2, String com3, String res){
-        this.compa1=com1;
-        this.compa2=com2;
-        this.compa3=com3;
-        this.respu=res;
-        this.compara1.setText(compa1.toString());
-    }*/
+    public String claves[]= new String[4];
+   public Respuesta(String res,String com1,String com2, String com3){
+        this.claves[0]=res;
+        this.claves[1]=com1;
+        this.claves[2]=com2;
+        this.claves[2]=com3;
+    }
     public Respuesta(){}
 }
