96b8861d2e48bfe13eb0a0c657d078b109c04631
jhon.calderon <jhon.calderon@udea.edu.co>
2016-10-30 21:05:17 -0500

    Se incluyen fragments

--- SAapp/app/src/main/java/com/pmovil/ui/saapp/MainActivity.java
+++ SAapp/app/src/main/java/com/pmovil/ui/saapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.pmovil.ui.saapp;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.os.SystemClock;
@@ -17,13 +19,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public String entrada;
     public Respuesta res1;
     int i=0,ocu=-1;
+    boolean listen=false;
     @Override
     protected void onCreate(Bundle savedInstancestate) {
         super.onCreate(savedInstancestate);
         setContentView(R.layout.activity_main);
-        res1= new Respuesta("Cordial saludo, en Medellin se hacen Domicilios por $5000","domicilio","contraentrega","entrega");
+        res1= new Respuesta("Cordial saludo, en Medellin se hacen domicilios por $5000","domicilio","contraentrega","entrega");
         salida = (TextView) findViewById(R.id.muestra);
-        findViewById(R.id.imageButton).setOnClickListener(this);
+        findViewById(R.id.añadirRes).setOnClickListener(this);
         findViewById(R.id.imageButton2).setOnClickListener(this);
         res1.respuesta = (TextView) findViewById(R.id.textView);
         res1.compara1 = (TextView) findViewById(R.id.textView1);
@@ -37,7 +40,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void onPrimaryClipChanged() {
                 String newClip = mClipboardManager.getText().toString();
                 Toast.makeText(getApplicationContext(), newClip.toString(),  Toast.LENGTH_SHORT).show();
-                compara();
+                if (listen==false)
+                    compara();
+                else listen=false;
             }
         });
     }
@@ -45,8 +50,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.imageButton2:
-                compara();
+            case R.id.añadirRes:
+                //Paso 1: Obtener la instancia del administrador de fragmentos
+                FragmentManager fragmentManager = getFragmentManager();
+
+                //Paso 2: Crear una nueva transacción
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                //Paso 3: Crear un nuevo fragmento y añadirlo
+                ResFrag fragment = new ResFrag();
+                transaction.add(R.id.layoutprincipal,fragment);
+
+                //Paso 4: Confirmar el cambio
+                transaction.commit();
 
         }
     }
@@ -55,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         entrada = String.valueOf(clipboard.getText());
         ocu=-1;
         i=1;
-        while (ocu==-1 && i<=2){
+        while (ocu==-1 && i<=3){
             ocu = entrada.indexOf(res1.claves[i]);
             i++;
         }
@@ -63,16 +79,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             salida.setText(res1.claves[i-1]);
             ClipData clip = ClipData.newPlainText("text", res1.respuesta.getText().toString());
             clipboard = (ClipboardManager) this.getSystemService(CLIPBOARD_SERVICE);
-            mClipboardManager.removePrimaryClipChangedListener(new ClipboardManager.OnPrimaryClipChangedListener() {
-
-                @Override
-                public void onPrimaryClipChanged() {
-                    String newClip = mClipboardManager.getText().toString();
-                    Toast.makeText(getApplicationContext(), newClip.toString(),  Toast.LENGTH_SHORT).show();
-                    compara();
-                }
-            });
             clipboard.setPrimaryClip(clip);
+            listen=true;
             }
         else
             salida.setText("No Encontrado");
