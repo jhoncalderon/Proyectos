aa0a62fee565b8ba21e3d7ce0af8a636a825c103
jhon.calderon <jhon.calderon@udea.edu.co>
2016-10-31 08:14:56 -0500

    Se crea el fragment para los articulos del inventario

--- SAapp/app/src/main/java/com/pmovil/ui/saapp/InventarioActivity.java
+++ SAapp/app/src/main/java/com/pmovil/ui/saapp/InventarioActivity.java
@@ -1,13 +1,39 @@
 package com.pmovil.ui.saapp;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class InventarioActivity extends AppCompatActivity {
-
+public class InventarioActivity extends AppCompatActivity implements View.OnClickListener,InvFrag.OnFragmentInteractionListener {
+    Button btnadd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventario);
+        btnadd=(Button)findViewById(R.id.btnInventario);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fragmentManager = getSupportFragmentManager ();
+
+        //Paso 2: Crear una nueva transacción
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        //Paso 3: Crear un nuevo fragmento y añadirlo
+        InvFrag fragment = new InvFrag();
+        transaction.add(R.id.layoutInventario,fragment);
+
+        //Paso 4: Confirmar el cambio
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
